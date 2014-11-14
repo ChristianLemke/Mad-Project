b755643e6920d5e5827b4ccab086769f4fcf7054
Jasmin91 <Jasmin.Profus@yahoo.de>
2014-11-14 02:17:42 -0800

    Grundlayout und Grundfunktionen Restaurant implementiert fehlen: aktueller Wechselkurs, shuffle button, Hintergrundbild

--- USASurvivalApp/gen/com/example/usasurvivalapp/R.java
+++ USASurvivalApp/gen/com/example/usasurvivalapp/R.java
@@ -25,9 +25,25 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int action_settings=0x7f080012;
+        public static final int euro_value=0x7f080009;
+        public static final int input_customtip=0x7f080007;
+        public static final int input_restaurant=0x7f080003;
         public static final int pager=0x7f080000;
+        public static final int radioGroup_tips=0x7f08000d;
+        public static final int radio_customTip=0x7f080011;
+        public static final int radio_tip15=0x7f08000e;
+        public static final int radio_tip18=0x7f080010;
+        public static final int radio_tip20=0x7f08000f;
         public static final int section_label=0x7f080001;
+        public static final int text_dollar1=0x7f080004;
+        public static final int text_dollar2=0x7f08000b;
+        public static final int text_dollar3=0x7f08000a;
+        public static final int text_euro=0x7f080002;
+        public static final int text_tips=0x7f08000c;
+        public static final int text_total=0x7f080006;
+        public static final int tips_value=0x7f080005;
+        public static final int total_value=0x7f080008;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
