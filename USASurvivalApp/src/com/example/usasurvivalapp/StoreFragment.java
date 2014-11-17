f86148341fa33f1044e99ccdfc8ab27149c18480
Jasmin91 <Jasmin.Profus@yahoo.de>
2014-11-16 22:33:27 -0800

    Currency, Measure und Sizes. Grundfunktionen soweit fertig, Wechselkurs muss noch akualisiert werden

--- USASurvivalApp/src/com/example/usasurvivalapp/StoreFragment.java
+++ USASurvivalApp/src/com/example/usasurvivalapp/StoreFragment.java
@@ -1,10 +1,13 @@
 package com.example.usasurvivalapp;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class StoreFragment extends Fragment {
 	/**
@@ -13,6 +16,10 @@ public class StoreFragment extends Fragment {
 	 */
 	private static final String ARG_SECTION_NUMBER = "section_number";
 	public static final int ARG_SECTION_NUMBER_STORE = 1;
+	Button btn_currency;
+	Button btn_sizes;
+	Button btn_measure;
+	
 
 	/**
 	 * Returns a new instance of this fragment for the given section number.
@@ -33,6 +40,41 @@ public class StoreFragment extends Fragment {
 			Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_store, container,
 				false);
+		
+		btn_currency = (Button) rootView.findViewById(R.id.btn_currency);
+		btn_sizes = (Button) rootView.findViewById(R.id.btn_sizes);
+		btn_measure = (Button) rootView.findViewById(R.id.btn_measure);
+		
+		btn_currency.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(getActivity(), CurrencyActivity.class);
+				startActivity(intent);
+				
+			}
+			
+		});
+		
+		btn_sizes.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(getActivity(), SizeActivity.class);
+				startActivity(intent);
+			}
+			
+		});
+		
+		btn_measure.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(getActivity(), MeasureActivity.class);
+				startActivity(intent);
+			}
+			
+		});
 		return rootView;
 	}
 }
